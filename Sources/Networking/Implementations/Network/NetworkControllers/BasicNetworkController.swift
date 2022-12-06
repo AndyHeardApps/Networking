89efe64bb202dfb36768bf029a3bae3ec96a4dee
Andy Heard <a.heard@cmcmarkets.com>
2022-12-06 12:56:54 +0000

    Added final source documentation

--- Sources/Networking/Implementations/Network/NetworkControllers/BasicNetworkController.swift
+++ Sources/Networking/Implementations/Network/NetworkControllers/BasicNetworkController.swift
@@ -1,21 +1,40 @@
 import Foundation
 
+/// A basic implementation of a ``NetworkController``, with no authorization on requests. This type will submit requests using the provided ``NetworkSession`` and transform responses using the ``NetworkRequest/transform(data:statusCode:using:)`` function.
+///
+/// Any request errors are handed to the ``errorHandler`` to enable more information to be extracted where possible before throwing the error.
+///
+/// The ``universalHeaders`` property can be used to add a static set of headers to every request submitted, such as API keys.
+///
+/// Though the implementation is intentionally lightweight, it is best if an instance is created once for each `baseURL` on app launch, and held for reuse.
 public struct BasicNetworkController {
     
     // MARK: - Properties
     
+    /// The base `URL` to submit all requests to. This is the base `URL` used to construct the full `URL` using the ``NetworkRequest/pathComponents`` and ``NetworkRequest/queryItems`` of the request.
     public let baseURL: URL
     
+    /// The ``NetworkSession`` used to fetch the raw `Data` ``NetworkResponse`` for a request.
     public let session: NetworkSession
     
+    /// The ``DataDecoder`` provided to a submitted ``NetworkRequest`` for decoding. It is best to set up a decoder suitable for the API once and reuse it. The ``NetworkRequest`` may still opt not to use this decoder.
     public let decoder: DataDecoder
         
+    /// The type used to handle any errors that are thrown by the ``NetworkRequest/transform(data:statusCode:using:)`` function of a request. This is used to try and extract error messages from the response if possible. If this property is `nil` then the unaltered error is thrown.
     public let errorHandler: NetworkErrorHandler?
 
+    /// The headers that will be applied to every request before submission.
     public let universalHeaders: [String : String]?
 
     // MARK: - Initialisers
-
+    
+    /// Creates a new ``BasicNetworkController`` instance.
+    /// - Parameters:
+    ///   - baseURL: The base `URL` of the controller.
+    ///   - session: The ``NetworkSession`` the controller will use.
+    ///   - decoder: The ``DataDecoder`` the controller will hand to requests for decoding.
+    ///   - errorHandler: The ``NetworkErrorHandler`` that can be used to manipulate errors before they are thrown.
+    ///   - universalHeaders: The headers applied to every request submitted.
     public init(
         baseURL: URL,
         session: NetworkSession = URLSession.shared,
--- Sources/Networking/Implementations/Network/NetworkControllers/ReauthorizingNetworkController.swift
+++ Sources/Networking/Implementations/Network/NetworkControllers/ReauthorizingNetworkController.swift
@@ -1,23 +1,46 @@
 import Foundation
 
+/// A ``NetworkController`` that authorizes every request submitted using the provided ``ReauthorizationProvider``, and attempts to reauthorize the app whenever authorization fails.
+///
+/// This type extends the authorizing behavior of the ``AuthorizingNetworkController``, so refer to its documentation for authorization details.
+///
+/// This difference between ``AuthorizingNetworkController`` and ``ReauthorizingNetworkController`` is that the ``errorHandler`` is a ``ReauthorizationNetworkErrorHandler`` that has the additional ``ReauthorizationNetworkErrorHandler/shouldAttemptReauthorization(afterCatching:from:)`` function that decides whether a thrown error can be recovered by reauthorizing and resubmitting the request. In addition, the ``authorization`` is a ``ReauthorizationProvider``, that provides the ``ReauthorizationProvider/makeReauthorizationRequest()`` and ``ReauthorizationProvider/handle(reauthorizationResponse:from:)`` functions for creating reauthorizing requests and handling their responses.
+///
+/// As with the ``AuthorizingNetworkController``, requests are handed to the ``AuthorizationProvider/authorize(_:)`` function before they are submitted, and instances of ``AuthorizationProvider/AuthorizationRequest`` and assocated ``NetworkResponse`` from successful requests are passed to the ``AuthorizationProvider/handle(authorizationResponse:from:)`` function.
+///
+/// If the requests ``NetworkRequest/transform(data:statusCode:using:)`` function throws an error and the ``errorHandler`` is not `nil`, it is passed to the ``ReauthorizationNetworkErrorHandler/shouldAttemptReauthorization(afterCatching:from:)`` function. If it returns `true` then the ``ReauthorizationProvider/makeReauthorizationRequest()`` function is used to create and submit a reauthorizing request. The initial failed request then has the updated credentials added to it and is resubmitted. If the ``errorHandler`` is nil, then this same logic is applied for a ``HTTPStatusCode/unauthorized`` status code by default.
 public struct ReauthorizingNetworkController<Authorization: ReauthorizationProvider> {
     
     // MARK: - Properties
     
+    /// The base `URL` to submit all requests to. This is the base `URL` used to construct the full `URL` using the ``NetworkRequest/pathComponents`` and ``NetworkRequest/queryItems`` of the request.
     public let baseURL: URL
     
+    /// The ``NetworkSession`` used to fetch the raw `Data` ``NetworkResponse`` for a request.
     public let session: NetworkSession
     
+    /// The ``ReauthorizationProvider`` used to authorize requests that need it, and reauthorize the app whenever possible.
     public let authorization: Authorization
     
+    /// The ``DataDecoder`` provided to a submitted ``NetworkRequest`` for decoding. It is best to set up a decoder suitable for the API once and reuse it. The ``NetworkRequest`` may still opt not to use this decoder.
     public let decoder: DataDecoder
     
+    /// The type used to handle any errors that are thrown by the ``NetworkRequest/transform(data:statusCode:using:)`` function of a request. This is used to decide whether or not to try and reauthorize the app. If not, then it will try and extract error messages from the response if possible. If this property is `nil` then the reauthorizing flow is triggered for a ``HTTPStatusCode/unauthorized`` status code, and unaltered errors are thrown if it fails.
     public let errorHandler: ReauthorizationNetworkErrorHandler?
     
+    /// The headers that will be applied to every request before submission.
     public let universalHeaders: [String : String]?
     
     // MARK: - Initialisers
 
+    /// Creates a new ``ReauthorizingNetworkController`` instance.
+    /// - Parameters:
+    ///   - baseURL: The base `URL` of the controller.
+    ///   - session: The ``NetworkSession`` the controller will use.
+    ///   - authorization: The ``ReauthorizationProvider``to use to authorize requests.
+    ///   - decoder: The ``DataDecoder`` the controller will hand to requests for decoding.
+    ///   - errorHandler: The ``ReauthorizationNetworkErrorHandler`` that can be used to manipulate errors before they are thrown, and decide whether reauthorization should be attempted.
+    ///   - universalHeaders: The headers applied to every request submitted.
     public init(
         baseURL: URL,
         session: NetworkSession = URLSession.shared,
